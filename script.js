f8ac034c1aa54228250ea6953b2cf3cf884c2786
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-19 13:40:19 +0200

    cleaned the code

--- script.js
+++ script.js
@@ -13,27 +13,25 @@ const nomiEstratti = [];
 btn.addEventListener('click', ()=>{
     btn.innerHTML = "Estrai"
     while(!estratto){
-        i = parseInt(Math.random()*listaNomi.length)
-        console.log(i)
-        let nomeEstratto = listaNomi[i]
+        i = parseInt(Math.random()*listaNomi.length);
+        console.log(i);
+        let nomeEstratto = listaNomi[i];
         console.log(nomeEstratto);
         
-
         if(!(nomiEstratti.length === listaNomi.length)){
             while(nomiEstratti.includes(nomeEstratto)){
-                i = parseInt(Math.random()*listaNomi.length)
+                i = parseInt(Math.random()*listaNomi.length);
                     console.log("nome era estratto");
-                    nomeEstratto = listaNomi[i]
-                    
-                    
+                    nomeEstratto = listaNomi[i];
             }
             nomiEstratti.push(listaNomi[i]);
-            h1.innerHTML = `Nome estratto: ${listaNomi[i]}`
+            h1.innerHTML = `Nome estratto: ${listaNomi[i]}`;
+            
         }else{
             h1.innerHTML = "tutti i nomi sono stati estratti";
-            btn.innerHTML = "ricomincia"
+            btn.innerHTML = "ricomincia";
             nomiEstratti.length = 0;
-            console.log(nomiEstratti, "adesso")
+            console.log(nomiEstratti, "adesso");
         }
         
         estratto = true;
